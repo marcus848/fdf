21ebae9be9cf337dd1529e87aa9d43981bfb2667
Marcus Vinicius Dos Santos Gomes <marcudos@c1r4p4.42sp.org.br>
2024-12-28 14:36:48 -0300

    first commit

---
+++ Makefile
@@ -0,0 +1,41 @@
+# File name
+NAME = project_name
+
+# Compiler and flags
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -g -Iinclude
+
+# Directories
+SRC_DIR = src
+OBJ_DIR = obj
+LIBFT_DIR = libft
+
+# Source and object files
+SRCS = $(wildcard $(SRC_DIR)/*.c)
+OBJS = $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+LIBFT = $(LIBFT_DIR)/libft.a
+
+# targets
+all: $(NAME)
+
+$(NAME): $(OBJS) $(LIBFT)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) -L$(LIBFT_DIR) -lft
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	@rm -rf $(OBJ_DIR) $(LIBFT_DIR)/obj
+	@echo "Object files removed."
+
+fclean: clean
+	@rm -f $(NAME) $(LIBFT)
+	@echo "fcleaned"
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ libft/include/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcudos <marcudos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 19:18:15 by marcudos          #+#    #+#             */
+/*   Updated: 2024/11/06 20:16:12 by marcudos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "ft_string.h"
+# include "ft_memory.h"
+
+char	*get_next_line(int fd);
+char	*find_next_line(char *line);
+char	*find_current_line(char *line);
+char	*get_full_line(int fd, char *line);
+
+#endif
